b972ca01deb31752f591ccf4494a0733e2ec049d
Sarthak03-bit <goelsarthak09@gmail.com>
2025-11-25 21:54:58 +0530

    database models updated

--- database_models.py
+++ database_models.py
@@ -1,13 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import mapped_column 
+from sqlalchemy.orm import mapped_column , Mapped
 
 Base = declarative_base()
 
 
 class Note(Base):
     __tablename__ = "Notes"
-    id = mapped_column(Integer , primary_key=True, index=True)
-    heading = mapped_column(String)
-    pointers = mapped_column(String)
+    id : Mapped[int]= mapped_column(Integer , primary_key=True, index=True)
+    heading: Mapped[str] = mapped_column(String)
+    pointers: Mapped[str] = mapped_column(String)
     
